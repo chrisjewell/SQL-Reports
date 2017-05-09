614fb5923ec23877c1a8df83302ddcd1852a2dba
chrisjewell <chrisjames.jewell@gmail.com>
2017-05-09 15:35:59 -0400

    Testing - MAY REVERT!
    Trying to "force" the Transactions table value for PLB tickets into
    TransactionDistribution table.
    I've made some changes to the joins in one of the subqueries, so I may
    need to revert if this ends up breaking the query farther down the road.

--- Financials/SQL - Revenue Journal Entry Report.sql
+++ Financials/SQL - Revenue Journal Entry Report.sql
@@ -158,7 +158,7 @@ WITH Resources AS
 					, NonCollectableInsuranceAdjustment = SUM(
 						CASE 
 						WHEN t.Action = 'A' AND pm.Source = 2 AND ISNULL(ml.FunctionName,'Y') = 'N'  THEN td.Amount
-						WHEN (t.Action = 'A' AND pm.Source = 2 AND ISNULL(ml.FunctionName,'Y') = 'N' AND temp.Code = 'PLB') THEN  t.Amount -- Makes PLB adjustment amounts NonCollectableInsuranceAdjustments
+						WHEN t.Action = 'A' AND pvp.Code = 'PLB' THEN  t.Amount -- Makes PLB adjustment amounts NonCollectableInsuranceAdjustments -- (pm.Source = 2 AND ISNULL(ml.FunctionName,'Y') = 'N' AND 
 						ELSE 0 
 						END)
 					, CollectablePatientAdjustment = SUM(
@@ -177,7 +177,7 @@ WITH Resources AS
 								, pvpa.InsAllocation AS InsuranceCharges
 								, pvpa.PatAllocation AS PatientCharges
 								, pvp.TotalAllowed AS Allowed
-								, pvp.Code AS Code
+								--, pvp.Code AS Code
 
 							FROM     PatientVisitProcs AS pvp 
 								JOIN PatientVisitProcsAgg AS pvpa 
@@ -197,10 +197,12 @@ WITH Resources AS
 
 					INNER JOIN TransactionDistributions AS td 
 						ON temp.PatientVisitProcsId = td.PatientVisitProcsId
-					INNER JOIN Transactions AS t 
-						ON t.TransactionsId = td.TransactionsId
+					inner join PatientVisitProcs as pvp
+						on temp.PatientVisitProcsId = pvp.PatientVisitProcsId
 					INNER JOIN VisitTransactions AS vt 
-						ON t.VisitTransactionsId = vt.VisitTransactionsId
+						ON pvp.PatientVisitId = vt.PatientVisitId
+					INNER JOIN Transactions AS t 
+						ON vt.VisitTransactionsId = t.VisitTransactionsId
 					INNER JOIN PaymentMethod AS pm 
 						ON vt.PaymentMethodId = pm.PaymentMethodId
 					INNER JOIN Batch AS b 
